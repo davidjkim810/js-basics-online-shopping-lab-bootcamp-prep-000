b5f99f81e410656b25cceb4f693cd343e0d0bf85
David Kim <davidjkim810@gmail.com>
2017-10-03 19:55:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ function setCart(c) {
   return cart;
 }
 
-function getRandomInt(min, max) {
+function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
 
 function addToCart(item) {
-  Object.assign({}, { [item]: `${getRandomArbitrary(0,100)}` });
+  Object.assign({}, { [item]: `${getRandomIntInclusive(1, 100)}` });
   console.log(`${item} has been added to your cart.`)
 return cart
 }
